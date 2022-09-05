dab9b414e07d494be0b521429cdb8f237b74d428
José Fernandes Rezende Neto <j.fr.neto@hotmail.com>
2022-09-05 17:56:28 -0300

    fixed cors

--- UniCashCRUDAPI/StudentApp/urls.py
+++ UniCashCRUDAPI/StudentApp/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from StudentApp import views
 
 urlpatterns = [
-    url(r'^student$',views.StudentAPI)
+    url(r'^student$',views.StudentAPI),
+    url(r'^student/([0-9]+)$',views.StudentAPI),
 ]
 
 
--- UniCashCRUDAPI/UniCashCRUDAPI/settings.py
+++ UniCashCRUDAPI/UniCashCRUDAPI/settings.py
@@ -38,16 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
+    "corsheaders",
     'StudentApp.apps.StudentappConfig'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
