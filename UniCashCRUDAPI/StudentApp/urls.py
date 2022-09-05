325930298b364289b1bb8d1ede0bf328520f2bd9
José Fernandes Rezende Neto <j.fr.neto@hotmail.com>
2022-09-05 17:58:21 -0300

    fix pathing

--- UniCashCRUDAPI/StudentApp/urls.py
+++ UniCashCRUDAPI/StudentApp/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 from StudentApp import views
 
 urlpatterns = [
-    url(r'^student$',views.StudentAPI),
-    url(r'^student/([0-9]+)$',views.StudentAPI),
+    re_path(r'^student$',views.StudentAPI),
+    re_path(r'^student/([0-9]+)$',views.StudentAPI),
 ]
 
 
